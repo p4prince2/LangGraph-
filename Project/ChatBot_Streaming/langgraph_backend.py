561357d38756cbc72ea51ac09584e27706a44447
Prince Kushwaha <134846426+p4prince2@users.noreply.github.com>
2025-10-03 10:11:05 +0530

    Streaming in chat

---
+++ Project/ChatBot_Streaming/langgraph_backend.py
@@ -0,0 +1,34 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm =  ChatOpenAI(
+    model="gpt-4o-mini",   # you can also use e.g. "aAnthropic/claude-3.5-sonnet"
+    base_url="https://openrouter.ai/api/v1",
+    temperature=0.7,api_key="sk-"
+)
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
+
+# Checkpointer
+checkpointer = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
