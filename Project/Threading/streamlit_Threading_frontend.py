d5758cd36b3567cca6da17f754b34aa2c5dca9e0
Prince Kushwaha <134846426+p4prince2@users.noreply.github.com>
2025-12-03 21:04:32 +0530

    Update streamlit_Threading_frontend.py

--- Project/Threading/streamlit_Threading_frontend.py
+++ Project/Threading/streamlit_Threading_frontend.py
@@ -4,7 +4,7 @@ from langchain_core.messages import HumanMessage, AIMessage
 import uuid
 
 # **************************************** utility functions *************************
-
+ 
 def generate_thread_id():
     thread_id = uuid.uuid4()
     return thread_id
@@ -98,3 +98,4 @@ if user_input:
 
 
     st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+
